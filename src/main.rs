d882a79b0e2caa5ca803349161b348e1ba50c0e6
Nicolas Arias <nicolas.arias@newshore.es>
2023-10-30 09:40:46 -0500

    Added port listening

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-fn main() {
-    let port = std::env::var("PORT");
-    println!("Hello, port {:?}!", port);
+use actix_web::{get, post, main, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+#[main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+        .bind(("127.0.0.1", std::env::var("PORT").unwrap_or_else(|_| "3030".to_string())))?
+        .run()
+        .await
 }
